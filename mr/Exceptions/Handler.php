36936f8b1ca0038af7dcd6da685c16a2afb6f345
Mosen <mosen@users.noreply.github.com>
2018-01-15 22:07:01 +1100

    test conversion for artisan

---
+++ mr/Exceptions/Handler.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Mr\Exceptions;
+
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
+}
