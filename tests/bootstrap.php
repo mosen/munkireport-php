2895227fcfc93fde13ad931c41ad5fd3c9394156
Mosen <mosen@users.noreply.github.com>
2018-07-08 21:58:26 +1000

    Re-add conf function

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,14 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../system/kissmvc.php');
 
 // Below are functions defined in index.php that are needed to run isolated controller tests.
+
+/**
+ * Get config item
+ * @param string config item
+ * @param string default value (optional)
+ * @author AvB
+ **/
+function conf($cf_item, $default = '')
+{
+    return array_key_exists($cf_item, $GLOBALS['conf']) ? $GLOBALS['conf'][$cf_item] : $default;
+}
